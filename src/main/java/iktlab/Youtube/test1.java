09a1e6eab7df44ee6ee58b9d96e396d073f35dc8
MuradIlyas <136617924+murad4312@users.noreply.github.com>
2023-08-09 18:47:55 +0400

    test comment forcommit
    .

---
+++ src/main/java/iktlab/Youtube/test1.java
@@ -0,0 +1,8 @@
+package iktlab.Youtube;
+
+public class test1 {
+    public static void main(String[] args) {
+        String str ="Hello world";
+        System.out.println("str = " + str);
+    }
+}
